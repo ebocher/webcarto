0ad873f55a53afc5009c6547469978283029c45c
Maxime TERRADE <maxime.terrade@eleves.ec-nantes.fr>
2013-04-12 15:53:25 +0200

    ZoomBoxFonctionnelle&AjoutCharsetUTF8

--- plugin_menu_droit/script/zoombox.js
+++ plugin_menu_droit/script/zoombox.js
@@ -1,43 +1,59 @@
-/*Zoom by Max*/
-	function modifBBox(coin1, coin2){ 
-//prend en argument les deux nouveaux coins de la BBox, de type LatLng
+	//Fonction qui recadre la map sur le rectangle dont les angles (diagonalement opposés) sont startLatLng et latlng
+function modifBBox(startLatLng, latlng){ 
+	//Création des variables (de type <Number>) latmin latmax lngmin lngmax
+	var latmin = Math.min(startLatLng.lat, latlng.lat),
+	latmax = Math.max(startLatLng.lat, latlng.lat) ,
+	lngmin = Math.min(startLatLng.lng, latlng.lng) ,
+	lngmax = Math.max(startLatLng.lng, latlng.lng) ;
 
-//Création des variables (de type <Number>) latmin latmax lngmin lngmax
-var latmin = Math.min(coin1.lat, coin2.lat),
-latmax = Math.max(coin1.lat, coin2.lat) ,
-lngmin = Math.min(coin1.lng, coin2.lng) ,
-lngmax = Math.max(coin1.lng, coin2.lng) ;
+	//Création des points pour définir la BBox puis création de la BBox 
+	var southWest = new L.LatLng(latmin, lngmin),
+		northEast = new L.LatLng(latmax, lngmax),
+		BBox = new L.LatLngBounds(southWest, northEast);
 
-//Création des points pour définir la BBox puis création de la BBox 
-var southWest = new L.LatLng(latmin, lngmin),
-    northEast = new L.LatLng(latmax, lngmax),
-    BBox = new L.LatLngBounds(southWest, northEast);
-
-/*modifier la carte pour afficher la BBox en argument (avec le zoom minimal. Donc si le rectangle sélectionné est plus haut que large, on verra exactement ce qui a été sélectionné en hauteur et un peu plus en largeur)*/
-map.fitBounds(BBox);
-
+	/*modifier la carte pour afficher la BBox en argument (avec le zoom minimal. Donc si le rectangle sélectionné est plus haut que large, on verra exactement ce qui a été sélectionné en hauteur et un peu plus en largeur)*/
+	map.fitBounds(BBox);
 }
+
+
+
+
 $('#zoom').click(function() {
 
-var coin1,coin2;
+	var startLatLng,latlng, shape;
+	map._container.style.cursor = 'crosshair';
 
-              //on ajoute l’EventListener suivant qui permettra de lancer la fonction si le bouton est enfoncé
-              map.addEventListener('mouseup', function(e) { 
-                         // e est l’objet (de type MouseEvent) renvoyé par l’évènement mouseup
-						 //Modification du position du cadre
-						 $(document).mousemove(function(e){
-      					 $( "#Cadre" ).css( "left", "e.pageX" );
-						 $( "#Cadre" ).css( "top", "e.pageY" );
-   }); 
+	//on ajoute l’EventListener suivant qui permettra de lancer la fonction si le bouton est enfoncé
+	map.addEventListener('click', function(e) { 
 
-                         coin1=e.latlng;
-           map.removeEventListener('mouseup') ; //on ne suit (=Listen) plus l’event mouseup 
-           map.addEventListener('mousedown', function(e2) {  //on suit l’event mousedown
-                                     coin2=e2.latlng;
-         map.removeEventListener('mousedown') ; //on ne suit plus l’event mousedown
-               //on dispose des deux coins => recadrage carte
-              		modifBBox(coin1, coin2) ;
-                          });	
-              });
+			 // e est l’objet (de type MouseEvent) renvoyé par l’évènement mouseup
+			 startLatLng=e.latlng;
+			 map.removeEventListener('click') ; //on ne suit (=Listen) plus l’event mouseup 
+			 
+			 //on suit l'event mousemove pour retracer le rectangle de séléction à chaque mouvement de souris
+			 map.addEventListener('mousemove', function(e3) {
+				latlng=e3.latlng;
+				//on trace le rectangle qui suit le curseur (on le crée s'il n'existe pas, sinon on le modifie)
+				if (!shape) {
+					shape = new L.Rectangle(new L.LatLngBounds(startLatLng, latlng), {color: '#007FFF', weight: 2, opacity: 0.8, fill: true, fillColor: '#FFFFFF',	fillOpacity: 0.6});
+					map.addLayer(shape);
+				} else {
+					shape.setBounds(new L.LatLngBounds(startLatLng, latlng));
+				}
+			 });
+			 
+			 //on suit l’event mousedown qui nous permet de savoir quand on a fini le recadrage
+			 map.addEventListener('click', function(e2) {  
+				latlng=e2.latlng;
+				map.removeEventListener('click') ; //on ne suit plus l’event mousedown
+				map.removeEventListener('mousemove') ; //on ne suit plus l’event mousedown
+				//on dispose des deux coins => recadrage carte et suppression du rectangle
+				map.removeLayer(shape);
+				modifBBox(startLatLng, latlng) ;
+				map._container.style.cursor = '';
+
+			 });	
+			 
+	});
 
 });
